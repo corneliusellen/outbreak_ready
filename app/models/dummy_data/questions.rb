38850a8868f534af2bfcea236f80ac100db63d37
corneliusellen <ecorneli@iwu.edu>
2020-07-11 17:00:16 -0600

    Add keys

--- app/models/dummy_data/questions.rb
+++ app/models/dummy_data/questions.rb
@@ -3,20 +3,24 @@ module DummyData
     def self.all
       [
         {
+          id: 1,
           text: "What is your race?",
           answer_type: :checkbox,
           answer_options: ['American Indian/Alaskan Native','Asian','Black or African American','Pacific Islander/Hawaiian Native','White','Other','Unknown','Decline to answer']
         },
         {
+          id: 2,
           text: "Did you attend the Piggly Wiggly tasting event?",
           answer_type: :radio,
           answer_options: ['Yes', 'No']
         },
         {
+          id: 3,
           text: "If yes, on which day?",
           answer_type: :text
         },
         {
+          id: 4,
           text: "How many other people did you eat with?",
           answer_type: :number
         }
