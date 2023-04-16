e7affdf5d10f0d64461bb0c8fee3c253dd7995b1
Phillip Strom <phillip.strom@gmail.com>
2023-04-16 09:14:16 -0600

    Add flash message styling

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,17 +1,18 @@
 module Admin
   class DashboardController < ApplicationController
     def index
-      @headers = ['ID','Question group','Section','Question','Mandatory','Field Type','Choices, Calculations, OR Slider Labels','Tag1','Tag2','Variable / Field Name','Form Name','SectionHeader','Field Label','Field Note','Text Validation Type OR Show Slider Number','Text Validation Min','Text Validation Max','Identifier?','Branching Logic (Show field only if...)','Required Field?','Custom Alignment','Question Number (surveys only)','Matrix Group Name','Matrix Ranking?','Field Annotation']
+      @headers = ['ID', 'Question group', 'Section', 'Question', 'Mandatory', 'Field Type', 'Choices, Calculations, OR Slider Labels', 'Tag1', 'Tag2',
+                  'Variable / Field Name', 'Form Name', 'SectionHeader', 'Field Label', 'Field Note', 'Text Validation Type OR Show Slider Number', 'Text Validation Min', 'Text Validation Max', 'Identifier?', 'Branching Logic (Show field only if...)', 'Required Field?', 'Custom Alignment', 'Question Number (surveys only)', 'Matrix Group Name', 'Matrix Ranking?', 'Field Annotation']
     end
 
     def upload
       result = Services::CsvImporter.new(csv_contents).import!
-      if result == "success"
-        flash[:notice] = "CSV import successful!"
+      if result == 'success'
+        flash[:success] = 'CSV import successful!'
       else
-        flash[:notice] = "CSV import failed: #{result}"
+        flash[:error] = "CSV import failed: #{result}"
       end
-        redirect_to action: :index
+      redirect_to action: :index
     end
 
     def sample_csv
@@ -19,7 +20,7 @@ module Admin
         format.csv do
           response.headers['Content-Type'] = 'text/csv'
           response.headers['Content-Disposition'] = 'attachment; filename=example_questions.csv'
-          render :template => "/admin/dashboard/example_csv.csv.erb"
+          render template: '/admin/dashboard/example_csv.csv.erb'
         end
       end
     end
